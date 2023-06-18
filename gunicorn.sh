68892b1beac9542bf5ae9bd9d04a3b750be3d8b5
RazielShushan <rzhtk151@gmail.com>
2023-06-18 22:03:15 +0300

    Setup Nginx

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source /var/lib/jenkins/workspace/django-cicd/env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/
+cd /var/lib/jenkins/workspace/django-cicd
 
 python3 manage.py makemigrations
 
