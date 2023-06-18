fbfe85f184a6d6616fa5b08fa05bf93608af813e
RazielShushan <rzhtk151@gmail.com>
2023-06-18 21:01:02 +0300

    Fix errors

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
 source env/lib/activate
-cd /var/lib/jenkins/workspace/djnago-cicd/
+
+cd /var/lib/jenkins/workspace/djnago-cicd/login_system
+
+python3 manage.py makemigrations
+
+python manage.py migrate
+
+echo "Migrations done" 
+
+cd /var/lib/jenkins/workspace/djnago-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
