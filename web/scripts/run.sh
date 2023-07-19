eb0bc137e1ce67895fc436157f927b96c7e40e0b
RazielShushan <rzhtk151@gmail.com>
2023-07-19 09:41:30 +0300

    jk

--- web/scripts/run.sh
+++ web/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-#/opt/venv/bin/python manage.py runserver
+/opt/venv/bin/python manage.py runserver '0.0.0.0:8002'
 #python manage.py collectstatic --noinput
 /opt/venv/bin/python manage.py makemigrations
 /opt/venv/bin/python manage.py migrate
