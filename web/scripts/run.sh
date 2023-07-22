43655f7859d04e6c061e9802664a79479f9a3abb
RazielShushan <rzhtk151@gmail.com>
2023-07-22 22:25:40 +0300

    k

--- web/scripts/run.sh
+++ web/scripts/run.sh
@@ -5,4 +5,4 @@ ls
 #python manage.py collectstatic --noinput
 /opt/venv/bin/python manage.py makemigrations
 /opt/venv/bin/python manage.py migrate
-/opt/venv/bin/gunicorn -b :8001 --chdir /app communication_system.wsgi:application
+/opt/venv/bin/gunicorn -b :8002 --chdir /app communication_system.wsgi:application
