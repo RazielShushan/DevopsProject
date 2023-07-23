6093e9b3c74b85efcf416bfad27d0c7dfbc714cd
RazielShushan <rzhtk151@gmail.com>
2023-07-23 20:12:37 +0300

    updtae

--- web/scripts/run.sh
+++ web/scripts/run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
-ls
-#/opt/venv/bin/python manage.py runserver '0.0.0.0:8002'
-#python manage.py collectstatic --noinput
 /opt/venv/bin/python manage.py makemigrations
 /opt/venv/bin/python manage.py migrate
 /opt/venv/bin/gunicorn -b :8002 --chdir /app communication_system.wsgi:application
